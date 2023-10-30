94768453781b34059933955dc88c3a3ccdc73661
Ehsan Abdolmajidi <ehsan.msc87@gmail.com>
2023-10-30 15:25:52 +0100

    Initial Commit

---
+++ Task4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task4
+//
+//  Created by Ehsan Abdolmajidi on 2023-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task4/Task4App.swift
@@ -0,0 +1,17 @@
+//
+//  Task4App.swift
+//  Task4
+//
+//  Created by Ehsan Abdolmajidi on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct Task4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
